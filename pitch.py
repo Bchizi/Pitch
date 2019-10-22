3bdac42f7d6596b3a2901b7852b897d31a9e1e2a
bchizi <leonbichanga@gmail.com>
2019-10-22 17:30:43 +0300

    succesfula validation of registration form and print an aleart message

--- pitch.py
+++ pitch.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for,flash,redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = '8842f7517ecf0393104bb6c30a420f74'
 
 posts = [
     {
@@ -29,6 +30,22 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About') 
+
+
+@app.route("/register" ,methods=['GET','POST'])
+def register():
+    form = RegistrationForm()    
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form=LoginForm()    
+    return render_template('login.html', title='Login', form=form)   
 
 
 if __name__ == '__main__':
