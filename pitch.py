904f329cf150a5df5756e15d2cdb9b15d655a61b
bchizi <leonbichanga@gmail.com>
2019-10-22 23:29:07 +0300

    linked diffren routes in the nav bar

--- pitch.py
+++ pitch.py
@@ -42,9 +42,16 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login",methods= ['GET','POST'])
 def login():
-    form=LoginForm()    
+    form=LoginForm()  
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('you have been logged in!', 'success')
+            return redirect(url_for('home'))  
+        else:
+            flash('log in unsuccefull','danger')
+
     return render_template('login.html', title='Login', form=form)   
 
 
