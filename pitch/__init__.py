e2e6df935e5cd53c9fab711731d589a333c76705
bchizi <leonbichanga@gmail.com>
2019-10-24 00:57:24 +0300

    added functionality to the form

--- pitch/__init__.py
+++ pitch/__init__.py
@@ -1,10 +1,14 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = '8842f7517ecf0393104bb6c30a420f74'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
+
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from pitch import views
--- pitch/forms.py
+++ pitch/forms.py
@@ -1,12 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired,Length,Email,EqualTo
+from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
+from pitch.models import User
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
     email = StringField('Email', validators=[DataRequired(),Email()])
     password = PasswordField('password',validators=[DataRequired()] )
     confirm_password = PasswordField('Confirm password',validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('username is taken please choose a different one')
+    
+    def validate_email(self, email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is taken please choose a different one')
+
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(),Email()])
